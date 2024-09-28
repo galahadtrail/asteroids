56c6bfe10acf5457118f1cecc0f557c664272fc1
galahadtrail <zzzzrzzzz@yandex.ru>
2024-09-28 14:26:33 +0300

    constructor added

--- player.py
+++ player.py
@@ -1,5 +1,8 @@
 from circleshape import *
+from constants import PLAYER_RADIUS
 
 
 class Player(CircleShape):
-    pass
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
