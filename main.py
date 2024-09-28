9ec3972345174ae4bb6d4f9bee70286d44ad86cc
galahadtrail <zzzzrzzzz@yandex.ru>
2024-09-28 14:47:05 +0300

    draw the rect now

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	py_time = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
 		screen.fill(BLACK)
+		player.draw(screen)
 		pygame.display.flip()
 		dt = py_time.tick(60) / 1000
-
+		
 
 if __name__ == "__main__":
 	main()
