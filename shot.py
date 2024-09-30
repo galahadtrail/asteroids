be9462caa012068d71fad1500f42e732c5761b4b
galahadtrail <zzzzrzzzz@yandex.ru>
2024-09-30 19:01:06 +0300

    new constant added

--- shot.py
+++ shot.py
@@ -3,7 +3,7 @@ from circleshape import *
 from constants import *
 
 
-class Asteroid(CircleShape):
+class Shot(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
